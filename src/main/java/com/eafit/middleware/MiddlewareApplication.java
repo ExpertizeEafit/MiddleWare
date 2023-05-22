c5562b1604473929ca445cc5357f4bb8559de439
= <=>
2023-05-22 18:30:49 -0500

    A dependencia has been removed

--- src/main/java/com/eafit/middleware/MiddlewareApplication.java
+++ src/main/java/com/eafit/middleware/MiddlewareApplication.java
@@ -2,10 +2,8 @@ package com.eafit.middleware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
-@EnableSpringHttpSession
 public class MiddlewareApplication {
 
 	public static void main(String[] args) {
